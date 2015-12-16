2bc8a208ce8ab328f2ea741e285b5c34b822bba7
Rye Skelton <rye@musubi00.local>
2015-12-15 17:55:55 -0800

    ajax cart part 2 - finished + additional requests

--- assets/swiper-main-frontpage.js
+++ assets/swiper-main-frontpage.js
@@ -6,7 +6,25 @@ var mySwiper = new Swiper ('.swiper-container', {
   loop: true,
   autoplay: 7000,
   // Default parameters
-    slidesPerView: 1,
+  slidesPerView: 1,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is <= 320px
+    320: {
+      
+    },
+    // when window width is <= 480px
+    480: {
+
+    },
+    // when window width is <= 640px
+    640: {
+
+    },
+    768: {
+
+    }
+  },
 
   // If we need pagination
   //pagination: '.swiper-pagination',
